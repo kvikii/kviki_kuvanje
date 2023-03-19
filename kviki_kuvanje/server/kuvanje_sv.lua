a197f7b705ff41bb08386ea5d58f75d6b578619d
kvikii <128297202+kvikii@users.noreply.github.com>
2023-03-19 15:42:10 +0100

    Update kuvanje_sv.lua

--- kviki_kuvanje/server/kuvanje_sv.lua
+++ kviki_kuvanje/server/kuvanje_sv.lua
@@ -7,11 +7,10 @@ print("kviki kao nesto kastm ukro vuletu skriptu")
 
 ESX.RegisterServerCallback('kviki:burgershot1', function(source,cb,item)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	-- if xPlayer.getInventoryItem('burger').count >= 1 and xPlayer.getInventoryItem('senf').count >= 1 and xPlayer.getInventoryItem('meso').count >= 1 then
-	if xPlayer.getInventoryItem('burger').count >= 1 then
+	if xPlayer.getInventoryItem('burger').count >= 1 and xPlayer.getInventoryItem('senf').count >= 1 and xPlayer.getInventoryItem('meso').count >= 1 then
 	     xPlayer.removeInventoryItem('burger',1)
-		-- xPlayer.removeInventoryItem('senf',1)
-		-- xPlayer.removeInventoryItem('meso',1)
+		xPlayer.removeInventoryItem('senf',1)
+		xPlayer.removeInventoryItem('meso',1)
     xPlayer.addInventoryItem('burger',1)
 		cb(true)
 	else
