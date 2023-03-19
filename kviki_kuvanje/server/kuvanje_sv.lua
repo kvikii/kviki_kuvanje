3e8559e953718b81ee1dd155fc64a1505644a0ad
Sarmingoo <78432708+Sarmingo@users.noreply.github.com>
2023-03-19 18:33:06 +0100

    Update kuvanje_sv.lua

--- kviki_kuvanje/server/kuvanje_sv.lua
+++ kviki_kuvanje/server/kuvanje_sv.lua
@@ -7,8 +7,7 @@ print("made by kviki#8628 & Sarmingooo#0850")
 
 ESX.RegisterServerCallback('kviki:burgershot1', function(source,cb,stahoces)
 	local xPlayer = ESX.GetPlayerFromId(source)
-		
-			for _,stahoces in pairs(Config.Burger) do
+			for groupName, stahoces in pairs(Config.Burger) do
 			for _,itemName in pairs(stahoces.items) do
 	if xPlayer.getInventoryItem(itemName).count >= 1 then
 		cb(true, stahoces)
